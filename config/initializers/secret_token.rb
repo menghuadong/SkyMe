bbeee156114210d020f97223d4baaa1aa7411070
menghuadong <huadongmeng@gmail.com>
2013-03-29 18:47:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkyMe::Application.config.secret_token = '580f7b4ff5435d68891324b9c05c3fe3f8ddd753681abcad2c60d9f5a57f7b0ed272a6ee635517c58ed14bc465ac0003ec92b2347c06c4bf21a810a17264a512'
