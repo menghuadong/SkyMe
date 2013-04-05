62e38500673f2bbd4d3ea9dd2ee117e9dfc1ee98
menghuadong <huadongmeng@gmail.com>
2013-04-05 18:33:59 +0800

    Review

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ActiveRecord::Base
   validates :password, :presence=> true, :length=> { :minimum=> 6 }
   validates :password_confirmation, :presence=> true
 
-
-
   private
 
     def create_remember_token
