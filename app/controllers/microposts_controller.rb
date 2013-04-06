fbe33f89530e66bfab7bf8d55b6bbd711df40f35
menghuadong <huadongmeng@gmail.com>
2013-04-06 15:58:48 +0800

    Fix bug

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -9,7 +9,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to users_path
+      redirect_to root_path
     else
       @feed_items = []
       render 'static_pages/home'
